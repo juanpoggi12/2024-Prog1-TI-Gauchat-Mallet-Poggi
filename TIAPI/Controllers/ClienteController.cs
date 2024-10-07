8da02342a1900df8a5cfa2a072af4540348668e1
gabrielmallet7 <mallet.gabriel2@gmail.com>
2024-10-07 13:57:19 -0300

    prueba commit

--- TIAPI/Controllers/ClienteController.cs
+++ TIAPI/Controllers/ClienteController.cs
@@ -8,50 +8,48 @@ namespace TIAPI.Controllers
     [Route("[controller]")]
     public class ClienteController : ControllerBase
     {
-        
-            private ClienteService clienteService;
-            public ClienteController()
+        private ClienteService clienteService;
+        public ClienteController()
+        {
+            clienteService = new ClienteService();
+        }
+        [HttpPost()]
+        public IActionResult Post([FromBody] ClienteDTO clienteDTO)
+        {
+            var Result = clienteService.AgregarCliente(clienteDTO);
+            if (Result.Success == false)
             {
-                clienteService = new ClienteService();
+                return BadRequest(Result.Message);
             }
-            [HttpPost()]
-            public IActionResult Post([FromBody] ClienteDTO clienteDTO)
+            return Ok();
+        }
+
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            var Result = clienteService.ObtenerClientes();
+            return Ok(Result);
+        }
+        [HttpDelete("{Dni}")]
+        public IActionResult Delete(int DNI)
+        {
+            var Result = clienteService.EliminarCliente(DNI);
+            if (!Result.Success)
             {
-                var Result = clienteService.AgregarCliente(clienteDTO);
-                if (Result.Success == false)
-                {
-                    return BadRequest(Result.Message);
-                }
-                return Ok();
+                return NotFound(Result.Message);
             }
 
-            [HttpGet()]
-            public IActionResult Get()
-            {
-                var Result = clienteService.ObtenerClientes();
-                return Ok(Result);
-            }
-            [HttpDelete("{Dni}")]
-            public IActionResult Delete(int DNI)
-            {
-                var Result = clienteService.EliminarCliente(DNI);
-                if (!Result.Success)
-                {
-                    return NotFound(Result.Message);
-                }
-
-                return Ok();
-            }
-            [HttpPut("{DNI}")]
-            public IActionResult Put(int DNI, [FromBody] ClienteDTO clienteDTO) { 
+            return Ok();
+        }
+        [HttpPut("{DNI}")]
+        public IActionResult Put(int DNI, [FromBody] ClienteDTO clienteDTO)
+        {
             var Resut = clienteService.EditarCliente(DNI, clienteDTO);
-                if (!Resut.Success)
-                {
-                    return BadRequest();
-                }
-                return Ok();
+            if (!Resut.Success)
+            {
+                return BadRequest();
             }
+            return Ok();
         }
     }
-
-
+}
