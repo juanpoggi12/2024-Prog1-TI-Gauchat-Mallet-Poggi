3ebbaa528558a44a574e2bf5230fc1a9459bffdb
gabrielmallet7 <mallet.gabriel2@gmail.com>
2024-10-08 15:30:01 -0300

    viajeController

---
+++ TIAPI/Controllers/ViajeController.cs
@@ -0,0 +1,29 @@
+﻿using EntitiesDTO;
+using Microsoft.AspNetCore.Mvc;
+using TIService;
+
+namespace TIAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ViajeController : Controller
+    {
+        private ViajeService viajeService;
+
+        public ViajeController()
+        {
+            viajeService = new ViajeService();
+        }
+
+        [HttpPost()]
+        public IActionResult Post([FromBody] ViajeDTO viajeDTO)
+        {
+            var result = viajeService.AgregarViaje(viajeDTO);
+            if (result.Success == false)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok();
+        }
+    }
+}
