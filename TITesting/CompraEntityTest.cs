bd2c108845dce6e86ef6619346eb52dd00c84407
gabrielmallet7 <mallet.gabriel2@gmail.com>
2024-10-19 13:29:13 -0300

    test2

---
+++ TITesting/CompraEntityTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+using NUnit.Framework;
+using TIEntities;
+
+namespace TITesting
+{
+    public class CompraEntityTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CompraOkey()
+        {
+            Compra compra = new Compra()
+            { 
+                Latitud = -35,
+                Longitud = -67
+            };
+
+            double resultado = compra.CalcularDistancia();
+
+            Assert.That(resultado, Is.EqualTo(661.18427631671841));
+        }
+    }
+}
